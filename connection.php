31fd94fdfd1c3702889eb41efb39133f110a0f4b
PuvaPipil <chayanonw@hotmail.com>
2017-03-11 14:52:35 +0700

    ming 1st update

---
+++ connection.php
@@ -0,0 +1,26 @@
+<?php
+$conn = mysql_connect("127.0.0.1","root","");
+mysql_select_db("student_enrolling_supporter");
+mysql_query("SET NAMES 'utf8' COLLATE 'utf8_general_ci';");
+?>
+
+
+<!--  <?php
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "student_enrolling_supporter";
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// } 
+// echo "Connected successfully";
+
+
+
+?>
+ -->
