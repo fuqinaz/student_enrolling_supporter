4aa7b22356035da68a3a8488bdf143ab5865d688
fuqinaz <fuqin.az@gmail.com>
2017-03-11 17:27:52 +0700

    merge schedule php and html files

--- login_handler.php
+++ login_handler.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 session_start();
 require("connection.php");
 
@@ -11,19 +12,27 @@ $result=mysql_query($query);
 
 if($list=mysql_fetch_array($result))
 {
+	// echo "if";
   	$_SESSION['userID']=$list['UserID'];
+  	// echo $_SESSION['userID'];
   	$_SESSION['fName']=$list['FName'];
   	$_SESSION['lName']=$list['LName'];
   	$_SESSION['majorCode']=$list['MajorCode'];
   	$_SESSION['attendYear']=$list['AttendYear'];
   	$_SESSION['userPic']=$list['Pic'];
   	$_SESSION['userType']=$list['UserType'];
+
    	header("refresh: 0; url=schedule.php");
+
+   	ob_end_flush();
 }
 
 else
 {
+	// echo "else";
 	header("refresh: 0; url=login.php");
+	//exit;
+	ob_end_flush();
 }
 
 
--- page_header.php
+++ page_header.php
@@ -3,6 +3,42 @@ require("check_login.php");
 ?>
 
 <HTML>
-<input type="button" onclick="window.location.href='schedule.php'" value="Schedule">
+<!-- <input type="button" onclick="window.location.href='schedule.php'" value="Schedule">
 <input type="button" onclick="window.location.href='grade_planner.php'" value="Grade">
+ -->
+    <!-- Web navigator -->
+    <div class="row">
+        <!-- <h3>This is Navigator Div</h3> -->
+        <div class="col-md-11">
+
+            <table class="navbar">
+                <tr>
+                    <td>
+                        <!-- <button type="button" class="btn btn-default"> -->
+                        <a href="schedule.php"><label>&nbsp;&nbsp;&nbsp;&nbsp;Scheduler&nbsp;&nbsp;&nbsp;&nbsp;</label></a>
+                        <!-- </button> -->
+                        
+                    </td>
+                    <td>
+                        <a href="grade_planner.html"><label>&nbsp;&nbsp;&nbsp;&nbsp;Grade Planner&nbsp;&nbsp;&nbsp;&nbsp;</label></a>
+                       <!-- <button type="button" class="btn btn-default" onclick="grade_planner.html"><label>Grade Planner</label></button> -->
+                    </td>
+                    <td>
+                        &nbsp;&nbsp;
+                        <a href="#">
+                            <span class="glyphicon glyphicon-user"></span>
+                        </a> 
+                        &nbsp;&nbsp;
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="col-md-1"></div>    
+        
+    </div>
+    <!-- End Web Nav -->
+
+
+
+
 </HTML>
